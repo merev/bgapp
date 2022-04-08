de8a54031d7bbbc5b6b280d548397f4a778748cc
merev <101055426+merev@users.noreply.github.com>
2022-04-08 15:42:33 +0300

    Update db_setup.sql

--- db/db_setup.sql
+++ db/db_setup.sql
@@ -4,7 +4,7 @@ grant all on bulgaria.* to 'web_user'@'%' identified by 'Password1';
 use bulgaria;
 create table cities (id int primary key auto_increment, city_name varchar(50), population int);
 INSERT INTO cities (city_name, population) VALUES ('София', 1236047);
-INSERT INTO cities (city_name, population) VALUES ('Пловдив', 343424);
+INSERT INTO cities (city_name, population) VALUES ('Trimontium', 343424);
 INSERT INTO cities (city_name, population) VALUES ('Варна', 335177);
 INSERT INTO cities (city_name, population) VALUES ('Бургас', 202766);
 INSERT INTO cities (city_name, population) VALUES ('Русе', 144936);
